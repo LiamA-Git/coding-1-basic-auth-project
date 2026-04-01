f7389ce39521752b70e4f9b26543c8564fb4add1
VondeT <s979083@student.knoxschools.org>
2026-04-01 14:12:52 +0000

    nothing

--- example/phase_2/phase_2.py
+++ example/phase_2/phase_2.py
@@ -28,14 +28,14 @@ base_style = """
 <style>
 body {
     font-family: Arial, sans-serif;
-    background: #f4f6f8;
+    background: ee__e;
     display: flex;
     justify-content: center;
     align-items: center;
     height: 100vh;
 }
 .card {
-    background: white;
+    background: black;
     padding: 25px;
     border-radius: 10px;
     box-shadow: 0 4px 10px rgba(0,0,0,0.1);
